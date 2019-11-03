e082aae4ef9245a6eeade37586dc423b34ab6721
nataliealway <nlmaxwell@gmail.com>
2019-11-03 10:17:14 -0800

    completed challenge 5

--- code-challenges/regular-expressions/challenges-04.test.js
+++ code-challenges/regular-expressions/challenges-04.test.js
@@ -82,7 +82,8 @@ The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "
 ------------------------------------------------------------------------------------------------ */
 
 const noPunctuation = str => {
-  // Solution code here...
+  let regex = /\w+\s/gi;
+  return str.match(regex);
 };
 
 /* ------------------------------------------------------------------------------------------------
