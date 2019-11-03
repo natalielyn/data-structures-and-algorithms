cbdd3f42419e2bf8234fbbd502eab14a08e63963
nataliealway <nlmaxwell@gmail.com>
2019-11-03 10:11:34 -0800

    Completed challenge 4

--- code-challenges/regular-expressions/challenges-04.test.js
+++ code-challenges/regular-expressions/challenges-04.test.js
@@ -63,7 +63,12 @@ Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
 const matchMonth = (input) => {
-  // Solution code here...
+  let regex = /^[oO]ct/
+  if (input.length === 5) {
+    return false;
+  } else {
+    return regex.test(input);
+  };
 };
 
 /* ------------------------------------------------------------------------------------------------
