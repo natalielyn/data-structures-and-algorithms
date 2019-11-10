e17f4207a26316cc420a5dec0c23172be5237836
nataliealway <nlmaxwell@gmail.com>
2019-11-10 15:31:21 -0800

    completed challenge 3

--- code-challenges/chainingMethods/challenges-10.test.js
+++ code-challenges/chainingMethods/challenges-10.test.js
@@ -55,7 +55,9 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(array => {
+    return array.filter(number => number % 5 === 0 && typeof number === 'number').map(number => Math.pow(2, number));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
