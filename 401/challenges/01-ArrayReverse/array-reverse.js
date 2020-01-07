0f65d1d1742f772ccd99a80ba3636045a0ae5b2f
natalielyn <nlmaxwell@gmail.com>
2020-01-06 18:51:13 -0800

    added code and updated readme

---
+++ 401/challenges/01-ArrayReverse/array-reverse.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const reverseArray = function(arr) {
+  const newArray = [];
+  let counter = 0;
+  for (let i = arr.length - 1; i > 0; i--) {
+    newArray[counter] = arr[i];
+    counter++;
+  }
+  return newArray;
+};
+
+module.exports = reverseArray;
